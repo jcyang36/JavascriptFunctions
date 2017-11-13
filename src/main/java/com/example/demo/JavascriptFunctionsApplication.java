7c4433d17f8d349543f9b7ba3f958bf92ba7e601
Joyce Yang <jcyang@hmc.edu>
2017-11-13 16:59:42 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/JavascriptFunctionsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptFunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavascriptFunctionsApplication.class, args);
+	}
+}
